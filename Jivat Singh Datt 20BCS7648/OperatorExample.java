b8c3ef13c3fb55463e48130c3336e71956027cb3
sHadowSparK <76220182+sHadowSparKti@users.noreply.github.com>
2021-09-18 10:21:36 +0530

    Add files via upload

---
+++ Jivat Singh Datt 20BCS7648/OperatorExample.java
@@ -0,0 +1,16 @@
+public class OperatorExample {
+
+        public static void main(String[] args)
+        {
+            int a,b;
+    
+            a = 10;
+            b = 5;
+    
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+        }
+}
+
